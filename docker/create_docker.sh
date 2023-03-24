0b46a6207a48aaa0d8c32d2151ebf61bba9985f3
Guido Schaefer <g.schaefer@cwi.nl>
2023-03-24 10:55:59 +0100

    updated docker files

--- docker/create_docker.sh
+++
@@ -1,2 +0,0 @@
-docker run -i -t -v /Users/schaefer/Research/2022/Dijkstra-predictions/branches/learning-in-dijkstra-b3:/home/learning-in-dijkstra dijkstra /bin/bash
-
---
+++ docker/run_docker.sh
@@ -0,0 +1,2 @@
+docker run -i -t -v /Users/schaefer/dijkstra-predictions-for-SSMTSP/:/home/dijkstra-predictions dijkstra /bin/bash
+
